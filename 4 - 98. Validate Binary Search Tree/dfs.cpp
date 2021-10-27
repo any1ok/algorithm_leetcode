f029e69e15e9a2c089c94ed5f61b12eae0416777
any1ok <any3ok@naver.com>
2021-10-27 17:56:31 +0900

    add

--- 4 - 98. Validate Binary Search Tree/dfs.cpp
+++ 4 - 98. Validate Binary Search Tree/dfs.cpp
@@ -15,9 +15,9 @@ public:
     }
 
     bool isValidBST(struct TreeNode* root) {
-        inorderTraversalR(root);
-        for (int i = 1; i < ans.size(); i++) {
-            if (ans[i - 1] >= ans[i])
+        inorderTraversalR(root); // 중위순열후
+        for (int i = 1; i < ans.size(); i++) { // 잘 sorting 되었는가
+            if (ans[i - 1] >= ans[i]) // [2,2,2] 같은거조심
                 return false;
         }
         return true;
